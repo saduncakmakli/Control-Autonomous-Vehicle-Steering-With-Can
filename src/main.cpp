083c955c494f1abafd7449c66fd20f450d7e0300
Muhammed Sadun Çakmaklı <muhammedsaduncakmakli@gmail.com>
2020-08-24 18:26:22 +0300

    encoder eklendi

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,103 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
-const int spiCSPin = 10;
+//UNO İCİN
+//MOSI 11 İLE KISA DEVRE!
+//MISO 12 İLE KISA DEVRE!
+//SCK 13 İLE KISA DEVRE!
+//ISCP ÜZERİNDEN SPI KULLANIRKEN BU PINLERI KULLANMA!
+//SMD OLMAYAN DIP SOKETLİ ARDUİNOLARDA 2.ICSP PINLARI 11,12,13DEN BAĞIMSIZ KULLANILABİLİR.
+//UNO PWM PINLER 3,5,6,9,10
+//UNO INTERRUPT PINLER 2,3
+
+//MEGA İCİN
+//MOSI 51
+//MISO 50
+//SCK 52
+//ISCP ÜZERİNDEN SPI KULLANIRKEN BU PINLERI KULLANMA!
+//MEGA PWM PINLER 2,3,4,5,6,7,8,9,10,11,12,13
+//13 ON BOARD LED
+//MEGA INTERRUPT PINLER INT0->2, INT1->3, INT2->21, INT3->20, INT4->19, INT5->18
+
+//SERIAL DEBUG ICIN 0,1 KULLANILIYOR BU PINLERI KULLANMA!
+
+const int spiCSPin = 10; //50 MISO, 51 MOSI, 52 SCK
+
+const int LIMIT_SOL_INTERRUPT_PIN = 21; //INT 2
+const int LIMIT_SAG_INTERRUPT_PIN = 20; //INT 3
+
+const int ENCODER_A_INTERRUPT_PIN = 19; //INT 4
+const int ENCODER_B_INTERRUPT_PIN = 18; //INT 5
+//const int ENCODER_Z_PIN = NULL;
+
+//LIMIT SWITLER BİR UCU SOL->20, SAG->21 DIGER UCU GND
 
 MCP_CAN CAN(spiCSPin);
 
+volatile long temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+
+void EncoderAInterrupt()
+{
+  // ai0 is activated if DigitalPin nr ENCODER_A_INTERRUPT_PIN is going from LOW to HIGH
+  // Check pin ENCODER_B_INTERRUPT_PIN to determine the direction
+  if (digitalRead(ENCODER_B_INTERRUPT_PIN) == LOW)
+  {
+    counter++;
+  }
+  else
+  {
+    counter--;
+  }
+}
+
+void EncoderBInterrupt()
+{
+  // ai0 is activated if DigitalPin nr ENCODER_B_INTERRUPT_PIN is going from LOW to HIGH
+  // Check with pin ENCODER_A_INTERRUPT_PIN to determine the direction
+  if (digitalRead(ENCODER_A_INTERRUPT_PIN) == LOW)
+  {
+    counter--;
+  }
+  else
+  {
+    counter++;
+  }
+}
+
+void kalibrasyon()
+{
+
+}
+
+void sag_limit()
+{
+  
+}
+
+void sol_limit()
+{
+
+}
+
 void setup()
 {
+  Serial.begin(9600);
+  pinMode(ENCODER_A_INTERRUPT_PIN, INPUT_PULLUP);
+  pinMode(ENCODER_B_INTERRUPT_PIN, INPUT_PULLUP);
+
+  //Setting up interrupt
+  //A rising pulse from encodenren activated EncoderAInterrupt(). AttachInterrupt ENCODER_A_INTERRUPT_PIN.
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A_INTERRUPT_PIN), EncoderAInterrupt, RISING);
+
+  //B rising pulse from encodenren activated EncoderBInterrupt(). AttachInterrupt ENCODER_B_INTERRUPT_PIN.
+  attachInterrupt(digitalPinToInterrupt(ENCODER_B_INTERRUPT_PIN), EncoderBInterrupt, RISING);
+
+  pinMode(LIMIT_SOL_INTERRUPT_PIN, INPUT_PULLUP);
+  pinMode(LIMIT_SAG_INTERRUPT_PIN, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(LIMIT_SOL_INTERRUPT_PIN), sol_limit, FALLING);
+  attachInterrupt(digitalPinToInterrupt(LIMIT_SAG_INTERRUPT_PIN), sag_limit, FALLING);
+
   Serial.begin(9600);
 
   while (CAN_OK != CAN.begin(CAN_500KBPS))
@@ -16,15 +107,23 @@ void setup()
     delay(100);
   }
   Serial.println("CAN BUS  Init OK!");
-}
 
+  kalibrasyon();
+}
 
 void loop()
 {
+  // Send the value of counter
+  if (counter != temp)
+  {
+    Serial.println(counter);
+    temp = counter;
+  }
+
   unsigned char len = 0;
   unsigned char buf[8];
 
-  if(CAN_MSGAVAIL == CAN.checkReceive())
+  if (CAN_MSGAVAIL == CAN.checkReceive())
   {
     CAN.readMsgBuf(&len, buf);
 
@@ -34,36 +133,16 @@ void loop()
     Serial.print("Data from ID: 0x");
     Serial.println(canId, HEX);
 
-    for(int i = 0; i<len; i++)
+    for (int i = 0; i < len; i++)
     {
       Serial.print(buf[i]);
       Serial.print(" | ");
     }
     Serial.println();
 
-    /*
-    if (canId == 21) // ÖN ULTRASONIK
-    {
-    unsigned int number[4];
-    number[0] = buf[0] | buf[1] << 8;
-    number[1] = buf[2] | buf[3] << 8;
-    number[2] = buf[4] | buf[5] << 8;
-    number[3] = buf[6] | buf[7] << 8;
-    Serial.print(number[0]);
-    Serial.print(" | ");
-    Serial.print(number[1]);
-    Serial.print(" | ");
-    Serial.print(number[2]);
-    Serial.print(" | ");
-    Serial.print(number[3]);
-    Serial.println();
-    }
-    */
-
     //MOTOR KONTROL
-    if (canId == 25)
+    if (canId == 250) //Direksiyon Açı bilgisi
     {
-      
     }
   }
 }
